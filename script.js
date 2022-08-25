f43816aab224cedba0c13655d06219a10498db7a
chaeyeon kim <kcy85078507@gmail.com>
2022-08-25 15:12:30 +0900

    index meta tag update

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let tabMenu = $(".tabmenu li"),
-  tabContent = $(".tabmenu__wrap"),
+  tabContent = $(".tabmenu__wrapper").children(),
   currentUrl = location.href; //현재 주소표시줄 주소 확인
 
 let dropdown = $(".dropdown");
 let dropdownMenu = $(".tab-menu2_1");
 let other = $(".product__page-container");
 
-// $(document).ready(function () {
 dropdown.click(function (e) {
   e.stopPropagation();
   e.preventDefault();
@@ -14,51 +13,44 @@ dropdown.click(function (e) {
 });
 
 $(document).on("click", function (e) {
-  // console.log(e.target);
-  // if (other.is(e.target) || $("#tab-menu1").is(e.target)) {
   dropdownMenu.hide();
-  // }
 });
+activateTab(0);
+
+// tabMenu.each(function () {
+//   let targetStr = $(this).find("a").attr("href");
+//   if (currentUrl.indexOf(targetStr) > -1) {
+//     let targetIdx = $(this).index();
+//     activateTab(targetIdx);
+//   }
+//   if (currentUrl.indexOf("#") == -1) {
+//     $(this).find("a").attr("href")
+//   }
 // });
 
-tabMenu.each(function () {
-  let targetStr = $(this).find("a").attr("href");
-  if (currentUrl.indexOf(targetStr) > -1) {
-    let targetIdx = $(this).index();
-    activateTab(targetIdx);
-  }
-  if (currentUrl.indexOf("#") == -1) {
-    activateTab(0);
-  }
-});
-
-console.log(currentUrl);
+// tabMenu.click(function (e) {
+//   e.preventDefault();
+//   // 이벤트를 막아줌
+//   activateTab($(this).index());
+// });
 
 tabMenu.click(function (e) {
   let targetIdx = $(this).index();
   e.preventDefault();
   // 이벤트를 막아줌
   activateTab(targetIdx);
-  console.log(tabContent.eq(targetIdx));
-
   if (currentUrl.indexOf("detail") > -1) {
     console.log("detail");
-    //   // $(location).attr("href", `product__page.html`);
-    //   // location.href = `product__page.html`;
-    //   console.log("fff");
+
     window.location.replace(`../product__page.html`);
     let targetIdx = $(this).index();
-    e.preventDefault();
-    // 이벤트를 막아줌
     activateTab(targetIdx);
-    console.log(tabContent.eq(targetIdx));
-    //   // $("body").css("backgroundColor", "red");
   }
 });
 
 function activateTab(idx) {
-  tabContent.find("a").removeClass("active");
-  tabContent.eq(idx).find("a").addClass("active");
+  tabContent.removeClass("active");
+  tabContent.eq(idx).addClass("active");
 
   tabMenu.removeClass("selected");
   tabMenu.eq(idx).addClass("selected");
