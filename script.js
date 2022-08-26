11b9c4079564f2b0af81c445dbe68cd12ad7d27b
chaeyeon kim <kcy85078507@gmail.com>
2022-08-26 09:55:58 +0900

    product__detail list selected update

--- script.js
+++ script.js
@@ -7,6 +7,28 @@ let dropdownMenu = $(".tab-menu2_1");
 let other = $(".product__page-container");
 
 $(function () {
+  tabMenu.each(function (index, item) {
+    $(item).click(function (e) {
+      if (currentUrl.indexOf("detail") > -1) {
+        e.preventDefault();
+        location.href = `product__page.html#${index}`;
+      } else {
+        e.preventDefault();
+        // 이벤트를 막아줌
+        // location.href = `product__detail.html#${index}`;
+        activateTab(index);
+      }
+    });
+  });
+
+  tabContent.each(function (index, item) {
+    $(item).click(function (e) {
+      console.log(index);
+      e.preventDefault();
+      location.href = `product__detail.html#${index}`;
+    });
+  });
+
   if (window.location.hash === "#0") {
     activateTab(0);
   }
@@ -39,40 +61,23 @@ dropdown.click(function (e) {
 $(document).on("click", function (e) {
   dropdownMenu.hide();
 });
-activateTab(0);
 
-// tabMenu.each(function () {
-//   let targetStr = $(this).find("a").attr("href");
-//   if (currentUrl.indexOf(targetStr) > -1) {
-//     let targetIdx = $(this).index();
-//     activateTab(targetIdx);
-//   }
-//   if (currentUrl.indexOf("#") == -1) {
-//     $(this).find("a").attr("href")
-//   }
-// });
+// activateTab(0);
 
 // tabMenu.click(function (e) {
-//   e.preventDefault();
-//   // 이벤트를 막아줌
-//   activateTab($(this).index());
-// });
+//   if (currentUrl.indexOf("detail") > -1) {
+//     e.preventDefault();
+//     let targetIdx = $(this).index();
+//     activateTab(2);
 
-tabMenu.click(function (e) {
-  if (currentUrl.indexOf("detail") > -1) {
-    // window.location.replace(`../product__page.html`);
-    // location.href = `product__page.html#tab-menu${targetIdx + 1}`;
-    e.preventDefault();
-    let targetIdx = $(this).index();
-    location.href = `product__page.html#${targetIdx}`;
-    // activateTab(targetIdx);
-  } else {
-    let targetIdx = $(this).index();
-    e.preventDefault();
-    // 이벤트를 막아줌
-    activateTab(targetIdx);
-  }
-});
+//     location.href = `product__page.html#${targetIdx}`;
+//   } else {
+//     let targetIdx = $(this).index();
+//     e.preventDefault();
+//     // 이벤트를 막아줌
+//     activateTab(targetIdx);
+//   }
+// });
 
 function activateTab(idx) {
   tabContent.removeClass("active");
